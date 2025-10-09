81829f1bdf1c73d7fdf666fa6d65e35a62f5981d
Anthony Hernandez <anthony.hernandez@utp.ac.pa>
2025-10-08 19:28:21 -0500

    subiendo ultimos cambios

--- 3D-Objects/tutorial/main.js
+++ 3D-Objects/tutorial/main.js
@@ -1,26 +1,16 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const fov = 75;
-const aspect = window.innerWidth / window.innerHeight;
-const near = 0.1;
-const far = 1000;
-const camera = new THREE.PerspectiveCamera(
-    fov,
-    aspect,
-    near,
-    far
-);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-//Cube
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material, 2 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
